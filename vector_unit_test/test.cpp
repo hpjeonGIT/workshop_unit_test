f2c278c831e17d2b042dfdb20dee86d9f9a5c698
Byoungseon Jeon <bjeon11@gmail.com>
2019-09-08 19:49:10 -0400

    Update test.cpp

--- vector_unit_test/test.cpp
+++ vector_unit_test/test.cpp
@@ -4,20 +4,30 @@
 using namespace testing;
 
 class MD_test: public Test{
+private:
 public:
-    Particle<double> *q = new Particle<double>(100);
+    Particle<double> q;
+    virtual void SetUp() {
+	std::cout << "now setup" << std::endl;
+	q = Particle<double>(100);
+    }
+    virtual void TearDown() {
+	std::cout << "now teardown" << std::endl;
+	// delete [] q; not necessary. Already deallocated
+    }
 };
 
 TEST_F(MD_test, basictest1) {
-    std::cout << q->xm << " jeonb " << std::endl;
-    ASSERT_THAT(q->xm,1.0);
+    EXPECT_THAT(q.xm,1.0);
+    std::cout << q.xm << " xm " << std::endl;
 }
 
 
 
 TEST_F(MD_test, basictest2) {
-    for(std::size_t i=0; i< 3; i++)
-	std::cout << q->xx[i] << " jeonb " << std::endl;
-    ASSERT_THAT(q->xx[0],1.0);
+    EXPECT_THAT(q.xx[99],1.0);
+    EXPECT_THAT(q.xx[89],1.0);
+   for(std::size_t i=0; i< 3; i++)
+	std::cout << q.xx[i] << " xx " << std::endl;
 }
 
