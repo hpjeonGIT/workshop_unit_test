8d6cc5277aa23aefc94b68531cb2c291fa52adb5
Byoungseon Jeon <bjeon11@gmail.com>
2019-09-08 20:21:45 -0400

    Update md_vector.h

--- md_vector.h
+++ md_vector.h
@@ -16,6 +16,28 @@ Particle(std::size_t Npt): xm(1.0), xx(Npt,1), xy(Npt,0), xz(Npt,0) {
     }
 };
 
+template <typename T>
+class ptclass {
+public:
+    T xx, xy, xz, xm;
+    ptclass() {
+	xx = 0.0;
+	xy = 1.0;
+	xz = 2.0;
+	xm = 11.1;
+    }
+    ~ptclass() {
+    }
+};
+
+template <typename T>
+struct ptstr {
+    T xx = 1.1;
+    T xy = 2.2;
+    T xz = 3.3;
+    T xm = 11.1;
+};
+
 /*
 template <typename T>
 class Particle {
