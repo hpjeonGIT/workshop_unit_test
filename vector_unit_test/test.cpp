4fbc961437e45e993aee94b9d8f82c8063de938f
Byoungseon Jeon <bjeon11@gmail.com>
2019-09-08 20:22:34 -0400

    Update test.cpp
    Instead of unique_ptr, let's use std::vector

--- vector_unit_test/test.cpp
+++ vector_unit_test/test.cpp
@@ -7,19 +7,22 @@ class MD_test: public Test{
 private:
 public:
     Particle<double> q;
+    std::vector<ptclass<double>> v;
     virtual void SetUp() {
-	std::cout << "now setup" << std::endl;
+	std::cout << "jeonb setup" << std::endl;
 	q = Particle<double>(100);
+	v.resize(100);
     }
     virtual void TearDown() {
-	std::cout << "now teardown" << std::endl;
+	std::cout << "jeonb teardown" << std::endl;
 	// delete [] q; not necessary. Already deallocated
     }
 };
 
 TEST_F(MD_test, basictest1) {
     EXPECT_THAT(q.xm,1.0);
-    std::cout << q.xm << " xm " << std::endl;
+    std::cout << q.xm << " jeonb " << std::endl;
+    EXPECT_THAT(v[75].xm ,11.1);
 }
 
 
@@ -28,6 +31,6 @@ TEST_F(MD_test, basictest2) {
     EXPECT_THAT(q.xx[99],1.0);
     EXPECT_THAT(q.xx[89],1.0);
    for(std::size_t i=0; i< 3; i++)
-	std::cout << q.xx[i] << " xx " << std::endl;
+	std::cout << q.xx[i] << " jeonb " << std::endl;
 }
 
