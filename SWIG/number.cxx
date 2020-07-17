1b6c9c0b3c74fdd5499fadf650f09052fa7b6bcf
Byoungseon Jeon <bjeon11@gmail.com>
2020-07-17 16:52:42 -0400

    testing derived classes

--- SWIG/number.cxx
+++ SWIG/number.cxx
@@ -1,6 +1,9 @@
 #include "number.h"
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <memory>
+#include <string>
 
 Number::Number(int start) { 
    data = start; 
@@ -34,4 +37,30 @@ void Number::display(  ) {
 
 void Number::timedata(std::time_t x) {
    std::cout << "time data = " << x << std::endl;
+}
+
+void Number::shtest(int value) {
+    n1 = std::make_shared<int>(value);
+    std::cout << n1.use_count() << " " << n1.get() <<std::endl;
+}
+
+void Parent::print_p() {
+    std::cout << "id_p=" << id_p << std::endl;
+}
+void Child::print_c() {
+    std::cout << "id_c=" << id_c << std::endl;
+}
+
+void mytest::print_mytest() {
+    abc.get()->print_c();
+}
+
+void mytest::activate(std::string txt) {
+    if (txt.compare("Parent") == 0) {
+	p1 = std::make_shared<Parent>(10);
+	std::cout << "p1 is activated\n";
+    } else {
+	c1 = std::make_shared<Child>(100);
+	std::cout << "c1 is activatged\n";
+    }
 }
