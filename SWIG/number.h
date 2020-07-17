f4720f7b806daa2ec04f54e75e13ca0391bdf2d8
Byoungseon Jeon <bjeon11@gmail.com>
2020-07-17 16:53:26 -0400

    Update number.h

--- SWIG/number.h
+++ SWIG/number.h
@@ -1,5 +1,8 @@
 //ref http://books.gigatux.nl/mirror/pythonprogramming/0596000855_python2-CHP-19-SECT-8.html
 #include <vector>
+#include <ctime>
+#include <memory>
+#include <string>
 class Number
 {
 public:
@@ -12,4 +15,42 @@ public:
     void timedata(std::time_t x);
     int data;
     std::vector<int> xyz;
+    void shtest(int value);
+    std::shared_ptr <int> n1;
+};
+
+class Parent
+{
+public:
+    int id_p;
+
+    Parent(int start) : id_p(start) {}
+    ~Parent()=default;
+
+    void print_p();
+};
+
+class Child : public Parent
+{
+public:
+    int id_c;
+    Child(int start) : id_c (start+10), Parent(start) {}
+    ~Child()=default;
+    void print_c();
+};
+
+class mytest
+{
+public:
+    int id_test;
+    std::shared_ptr<Child> abc;
+    std::shared_ptr<Parent> p1;
+    std::shared_ptr<Child> c1;
+    mytest(int start) {
+	id_test = start;
+	abc = std::make_shared<Child>(id_test);
+    }
+    ~mytest() = default;
+    void print_mytest();
+    void activate(std::string txt);
 };
